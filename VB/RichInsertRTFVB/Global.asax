683816057305d21e8eda2203115836bb865a6006
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-10-02 14:37:49 +0400

    Change example status to out of maintenance

--- VB/RichInsertRTFVB/Global.asax
+++
@@ -1,1 +0,0 @@
-<%@ Application Codebehind="Global.asax.vb" Inherits="RichInsertRTFVB.MvcApplication" Language="VB" %>
--- VB/RichInsertRTFVB/Models/RichEditData.vb
+++
@@ -1,7 +0,0 @@
-﻿Imports DevExpress.XtraRichEdit
-
-Public Class RichEditData
-    Public Property DocumentId As String
-    Public Property DocumentFormat As DocumentFormat
-    Public Property Document As Byte()
-End Class
